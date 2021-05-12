f357bea07e25dbc423707b0e0a9e154499129360
Adam Brieger <adam.brieger@gmail.com>
2021-05-12 11:10:11 +1000

    fix: updates change_ext.sh so that it is more robust when project dir includes subdirs

--- change_ext.sh
+++ change_ext.sh
@@ -1,42 +1,38 @@
 #!/bin/dash
 
-# Program changes extensions of files
-# Written by Adam Brieger on 24 March 2021
+# Program changes extensions of files in current directory
+# Written by Adam Brieger
 
-if test $# -eq 0
+if test $# -ne 0
 then
-    echo "usage: ./change_ext.sh <projectdir>" >&2
+    echo "usage: ./change_ext.sh" >&2
     exit 1
 fi
 
-read -p "Enter the old extension: " old_ext
-read -p "Enter the new extension: " new_ext
+read -p "Enter the old extension (excluding dot): " old_ext
+read -p "Enter the new extension (excluding dot): " new_ext
 
-for file in *
+for file in $(find * -type f)
 do
     test "$file" = "change_ext.sh" &&
         continue # Ignore this script
-    if test -d "$file" 
-    then
-        # If file is directory, run script inside directory
-        "$0" "$file"/*
-    else
-        echo "$file" | 
-        grep -E "${old_ext}$" | # Retrieve only files ending with ext
-        while read res
-        do
-            no_ext=$(echo $res | sed -E 's/\..*$//') # Delete old extension
-            new_filename=${no_ext}${new_ext} # Add new extension
 
-            if test -e "$new_filename"
-            then
-                echo "$new_filename already exists" >&2
-                continue
-            fi
+    echo "$file" | 
+    grep -E "${old_ext}$" | # Retrieve only files ending with ext
+    while read res
+    do
+        no_ext=$(echo $res | sed -E 's/\..*$/\./') # Delete old extension
+        new_filename=${no_ext}${new_ext} # Add new extension
 
-            echo "$file -> $new_filename"
-            mv -- "$file" "$new_filename" # Replace old file
-        done
+        if test -e "$new_filename"
+        then
+            # Do not overwrite existing files
+            echo "$new_filename already exists" >&2
+            continue
+        fi
 
-    fi
+        echo "$file -> $new_filename"
+        mv -- "$file" "$new_filename" # Replace old file
+    done
+
 done
