db6ab20bb4c24575d99ef2bfdaeda2ad1cddc4bf
Adam Brieger <adam.brieger@gmail.com>
2021-05-17 21:34:06 +1000

    feat: convert_case version 1 completed

--- convert_case.pl
+++ convert_case.pl
@@ -1,5 +1,6 @@
 #!/usr/bin/perl -w
 
+# Program creates new file with converted case
 # Option -s converts to snake case and -c converts to camel case
 # Assumes strings do not contain escaped quotations
 
@@ -8,6 +9,22 @@
 sub usage_exit {
     print STDERR "usage: $0 [ -s | -c ] <file>\n";
     exit 1;
+}
+
+sub convert_to_camel {
+    if (! $in_string) {
+        $word =~ s/_(.)/uc($1)/ge;
+    }
+}
+
+sub convert_to_snake {
+    if (! $in_string) {
+        @caps = ($word =~ /([A-Z])/g);
+        foreach $cap (@caps) {
+            $lower = lc $cap;
+            $word =~ s/$cap/_$lower/g;
+        }
+    }
 }
 
 # Only one option and one file to be provided in command line
@@ -20,9 +37,17 @@ getopts("sc");
 
 # Process file
 $file = shift @ARGV or usage_exit();
-open my $f, '<', $file or die "$0: Unable to open file $file\n";
-@lines = <$f>;
-close $f;
+open my $in, '<', $file or die "$0: Unable to open file $file\n";
+@lines = <$in>;
+close $in;
+
+# Prepare new file
+$new_file = "snake_" . $file if $opt_s;
+$new_file = "camel_" . $file if $opt_c;
+if (-e "$new_file" ) {
+    print STDERR "$new_file already exists!\n";
+    exit 1;
+}
 
 # Transform case
 foreach $line (@lines) {
@@ -32,37 +57,29 @@ foreach $line (@lines) {
 
     foreach $word (@words) {
 
+        # Do not transform strings
         if ($word =~ /^[^"']*("|')[^"']*$/ and ! $in_string) {
             # String begins with this word
-            # print("String opened with $word\n");
             $in_string = 1;
             $marker = $1; # Store style of quotation
-            next; # Do not transform this word
+            next; 
         } elsif ($word =~ /^[^"']*("|')[^"']*$/ and $in_string) {
             # String ends with this word
-            # print("String closed with $word\n");
             if ("$1" eq "$marker") {
                 $in_string = 0;
-                next; # Do not transform this word
+                next; 
             }
         }
 
-        if ($opt_c and ! $in_string) {
-            # Convert snake to camel
-            $word =~ s/_(.)/uc($1)/ge;
-        }
-
-        if ($opt_s and ! $in_string) {
-            # Convert camel to snake
-            @caps = ($word =~ /([A-Z])/);
-            foreach $cap (@caps) {
-                $lower = lc $cap;
-                $word =~ s/$cap/_$lower/g;
-            }
-        }
+        convert_to_camel() if $opt_c;
+        convert_to_snake() if $opt_s;
     }
     
     $line = join(' ', @words);
     print $line;
-
 }
+
+# Write to new file
+open my $out, '>', $new_file or die "$0: Unable to create file $new_file";
+print $out @lines;
+close $out;
